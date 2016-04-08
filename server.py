ba736b11b987de667cae15e7270f9a8b1e2832b1
Floris <sk8_floris@hotmail.com>
2016-04-08 11:26:02 +0200

    comments removed

--- server.py
+++ server.py
@@ -5,18 +5,12 @@ import re
 app = Flask(__name__)
 
 dict = {}  # carrying url id pairs
-# id_counter = 65
 global id_counter
 id_counter = 0
-id_char_num = 1
 
 
 def get_key(value):
-    # print "get_key: " + str(dict.items())
-    # print str(value)
     for key, val in dict.items():
-        # if str(dict[key]) == str(value):
-        # print str(val) + " == " + str(value)
         if str(val) == str(value):
             return key
     
@@ -26,9 +20,6 @@ def get_key(value):
 def gen_new_id():
     global id_counter
     newid = id_counter
-    # if id_counter < 91:
-    #     id_counter += 1
-    #     return chr(id)
     if id_counter < 10:
         id_counter += 1
         return newid
@@ -53,20 +44,15 @@ def id_handler(id):
     id = str(id) # get rid of the 'u' problem
     if request.method == 'GET':
         url = get_key(str(id))
-        # print url
         if url:
             return redirect(url, code=301)
         else:
             return ('', 404)
-            # abort(404) # causes 233 char body which ruby client doesnt want
+    
     if request.method == 'PUT':
         url = validate_url(str(request.form['url']))
-        # if id not in dict.viewvalues():
-            # print dict.viewvalues()
         if check_id(id) == 0:
-            # print "id not found"
             return ('', 404)
-            # abort(404) # causes 233 char body which ruby client doesnt want
         elif url:
             del dict[get_key(str(id))] # delete old entry
             dict[url] = str(id) #update dict with new url, keep old id
@@ -75,7 +61,6 @@ def id_handler(id):
             # There is no url in the request
             # Or id is non-existent
             return ('', 400)
-            # abort(400) # causes 233 char body which ruby client doesnt want
 
     if request.method == 'DELETE':
         for key, item in dict.items():
@@ -84,27 +69,20 @@ def id_handler(id):
                 return ('', 204)
         #item never existed in the first place
         return ('', 404)
-        # abort(404) # causes 233 char body which ruby client doesnt want
 
 
 @app.route('/', methods=['GET', 'POST', 'DELETE'])
 def url_handler():
     if request.method == 'GET':
-        # print jsonify(keys=dict.keys())
-        # print str(dict.keys())
-        # print dict.items()
         id_list = ""
         for id in dict.values():
             if id_list == "":
                 id_list += id
             else:
                 id_list += ","+id
-        # print id_list
         return make_response(id_list, 200)
-        # return make_response(str(dict.keys()), 200)
-        # return make_response(jsonify(keys=dict.keys()), 200)
+
     if request.method == 'POST':
-        # print dict.keys()
         url = validate_url(str(request.form['url']))
         if url:
             if url in dict.keys():
@@ -116,7 +94,7 @@ def url_handler():
                 return make_response(str(dict[url]), 201)
         else:
             return ('', 400)
-            # abort(400) # causes 233 char body which ruby client doesnt want
+
     if request.method == 'DELETE':
         dict.clear()
         return ('', 204)
